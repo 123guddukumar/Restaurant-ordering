d4966a2cfa8b214354817294a9f4b6e203b4bdb8
DivyansuVerma <130806575+Divyansuverma@users.noreply.github.com>
2025-09-18 03:57:50 +0530

    fix url error

--- backend/urls.py
+++ backend/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('orders.urls')),
-] + if settings.DEBUG:
+]
+
+if settings.DEBUG:  # condition alag likho
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
